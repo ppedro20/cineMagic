70da87fc3f8264aa93d8e62280213a3c8b423695
patinhooh <2222277@my.ipleiria.pt>
2024-06-14 19:04:43 +0100

    Side bar Screenings fix

--- resources/views/layouts/admin_partials/sidebar.blade.php
+++ resources/views/layouts/admin_partials/sidebar.blade.php
@@ -144,6 +144,30 @@
                         </x-menus.admin-group-menu-items>
                     @endcan
 
+                    @can('viewAny', App\Models\Screening::class)
+                    <!-- Screening -->
+                        @php
+                            $options = [];
+                            $options['All Screening'] = route('screenings.index');
+                        @endphp
+                    @if(auth()->user()->getTypeDescriptionAttribute() == 'Administrative')
+                        @can('create', App\Models\Screening::class)
+                            @php
+                                $options['Add Screening'] = route('screenings.create');
+                            @endphp
+                        @endcan
+                    @endif
+                        <x-menus.admin-group-menu-items title="Screening" :options="$options">
+                            <svg class="w-6 h-6 text-gray-400 dark:text-white" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                                viewBox="0 0 24 24">
+                                <path fill-rule="evenodd"
+                                    d="M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H6Zm7.25-2.095c.478-.86.75-1.85.75-2.905a5.973 5.973 0 0 0-.75-2.906 4 4 0 1 1 0 5.811ZM15.466 20c.34-.588.535-1.271.535-2v-1a5.978 5.978 0 0 0-1.528-4H18a4 4 0 0 1 4 4v1a2 2 0 0 1-2 2h-4.535Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </x-menus.admin-group-menu-items>
+                    @endcan
+
                     @can('update', App\Models\Configuration::class)
                     <!-- Configuration -->
                         @php
@@ -162,7 +186,7 @@
                             </svg>
                         </x-menus.admin-group-menu-items>
                     @endcan
-                    
+
                 @endauth
                     <!-- Only one option -->
                     <x-menus.admin-group-menu-items class="mt-2" title="Voltar ao site" :options="['Home' => route('home')]">
